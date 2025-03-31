eb029edd6353cc979ba469a1d34a84a758c8ac5e
CaOttaviano <cmottaviano@mail.austral.edu.ar>
2025-03-31 12:59:53 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,6 @@
 def change():
     expense = 23.75
     money = 100
+    
+    print (f"The result of {x} comes first in the dictionary than {y} is {x>y}.")
+    print (f"The result of {y} comes first in the dictionary than {x} is {y>x}.")
